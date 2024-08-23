e5afc8919a1228190872be254be744c7c9dc0790
kiarashfz <kiarshfz.fazli@gmail.com>
2024-08-23 07:13:27 +0330

    postgres setup

---
+++ pkg/adapters/storage/setup.go
@@ -0,0 +1,24 @@
+package storage
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"kia-logix/config"
+)
+
+func NewPostgresGormConnection(dbConfig config.DB) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		dbConfig.Host, dbConfig.User, dbConfig.Pass, dbConfig.DBName, dbConfig.Port)
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+func Migrate(db *gorm.DB) error {
+	migrator := db.Migrator()
+
+	err := migrator.AutoMigrate()
+	if err != nil {
+		return err
+	}
+	return nil
+}

1d87cd1a2b0b122c70ac20af8830f43eaddb037f
kiarashfz <kiarshfz.fazli@gmail.com>
2024-08-25 10:38:40 +0330

    varchar changed to 15

--- pkg/adapters/storage/entities/user.go
+++ pkg/adapters/storage/entities/user.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	gorm.Model
 	Name     string `gorm:"type:varchar(255);not null"`
-	Phone    string `gorm:"unique;type:varchar(20);not null"`
+	Phone    string `gorm:"unique;type:varchar(15);not null"`
 	Password string `gorm:"type:varchar(255);not null"`
 	IsAdmin  bool   `gorm:"not null"`
 }

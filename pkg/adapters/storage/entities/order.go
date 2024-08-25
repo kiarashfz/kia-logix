8d8f9cc3b35a8b4d2091c7d4f539c6f1e07c012a
kiarashfz <kiarshfz.fazli@gmail.com>
2024-08-25 10:44:03 +0330

    order model

---
+++ pkg/adapters/storage/entities/order.go
@@ -0,0 +1,24 @@
+package entities
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Order struct {
+	gorm.Model
+	OwnerID           uint
+	Owner             *User  `gorm:"foreignKey:OwnerID"`
+	SenderName        string `gorm:"not null; type:varchar(255)"`
+	SenderPhone       string `gorm:"not null; type:varchar(15)"`
+	SenderAddressID   uint
+	SenderAddress     *Address `gorm:"foreignKey:SenderAddressID"`
+	ReceiverName      string   `gorm:"not null; type:varchar(255)"`
+	ReceiverPhone     string   `gorm:"not null; type:varchar(15)"`
+	ReceiverAddressID uint
+	ReceiverAddress   *Address `gorm:"foreignKey:ReceiverAddressID"`
+	ProviderID        uint
+	Provider          *Provider `gorm:"foreignKey:ProviderID"`
+	PickupDate        time.Time
+	Status            string `gorm:"not null; type:varchar(63)"`
+}

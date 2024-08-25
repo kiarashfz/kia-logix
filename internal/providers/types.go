a75fb720c733765096f2e69d8728f190ff24813c
kiarashfz <kiarshfz.fazli@gmail.com>
2024-08-25 10:38:28 +0330

    domain provider

--- internal/providers/types.go
+++ internal/providers/types.go
@@ -2,9 +2,12 @@ package providers
 
 import (
 	"context"
+	"errors"
+	"net/url"
 )
 
 type Repo interface {
+	Create(ctx context.Context, provider *Provider) (*Provider, error)
 	GetAll(ctx context.Context, limit, offset uint) (providers []Provider, total uint, err error)
 }
 
@@ -12,4 +15,17 @@ type Provider struct {
 	ID   uint
 	Name string
 	URL  string
+}
+
+func ValidateURL(input string) error {
+	parsedURL, err := url.ParseRequestURI(input)
+	if err != nil {
+		return errors.New("invalid URL")
+	}
+
+	// Check if the scheme (protocol) and host are set in the parsed URL
+	if parsedURL.Scheme == "" || parsedURL.Host == "" {
+		return errors.New("invalid URL")
+	}
+	return nil
 }

988c95ac8731c4587cc3453edd08d2f50b858c8e
kiarashfz <kiarshfz.fazli@gmail.com>
2024-08-25 18:46:43 +0330

    order repo implementation

--- pkg/adapters/storage/order.go
+++ pkg/adapters/storage/order.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gorm.io/gorm"
 	"kia-logix/internal/orders"
+	"kia-logix/pkg/adapters/storage/entities"
 	"kia-logix/pkg/adapters/storage/mappers"
 )
 
@@ -25,4 +26,23 @@ func (o *orderRepo) Create(ctx context.Context, order *orders.Order) (*orders.Or
 	}
 	createdOrder := mappers.OrderEntityToDomain(newOrder)
 	return createdOrder, nil
+}
+
+func (o *orderRepo) Update(ctx context.Context, order *orders.Order) error {
+	var orderEntity *entities.Order
+	orderEntity = mappers.OrderDomainToEntity(order)
+	if err := o.db.WithContext(ctx).Model(&orderEntity).Where("id = ?", orderEntity.ID).Updates(map[string]interface{}{"status": orderEntity.Status, "is_picked_up_sms_sent": orderEntity.IsPickedUpSMSSent, "delivered_date": orderEntity.DeliveredDate}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *orderRepo) GetUpdateNeedOrders(ctx context.Context) ([]orders.Order, error) {
+	var orderEntities []entities.Order
+	err := o.db.WithContext(ctx).Preload("Provider").Preload("Owner").Where("status NOT IN ?", []string{string(orders.DELIVERED), string(orders.PENDING)}).Find(&orderEntities).Error
+	if err != nil {
+		return nil, err
+	}
+	orderDomainObjs := mappers.BatchOrderEntityToDomain(orderEntities)
+	return orderDomainObjs, nil
 }
